206fe7a4f442b1cb1a009b409ef5a6f1f25d531e
KevvLB <drleboedec@hotmail.fr>
2025-02-10 20:56:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -31,8 +31,6 @@ st.title("HARISS")
 file = st.file_uploader("upload excel file", type={"xlsx"})
 if file is not None:
     df = pd.read_excel(file)
-else:
-    df=np.zeros((1,1))
 st.write(df)
 
 # %% Projet 2_CNN model_HARISS_Shiny.ipynb 6
