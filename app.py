d9f29f8c769523a6af7bbebd2e304b45ec6b7686
KevvLB <drleboedec@hotmail.fr>
2025-02-10 21:21:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -82,8 +82,8 @@ for i in range(df.shape[1]):
     plt.axis('off')
     img_buf = io.BytesIO()
     plt.savefig(img_buf, format='jpg')
-    plt.show()
     hist.append(Image.open(img_buf))
+    print(plt.show())
 
 # %% Projet 2_CNN model_HARISS_Shiny.ipynb 11
 fig, axes = plt.subplots(nrows=1, ncols=len(hist))
