6660423b6ca413c107cf1a9721a148e8fe0c9063
KevvLB <drleboedec@hotmail.fr>
2025-02-13 13:16:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -239,4 +239,4 @@ for images in loader:
             upper90_low[i]=np.quantile(btupper, 0.05)
             upper90_up[i]=np.quantile(btupper, 0.95)
         st.image(hist[i], caption=df.columns[i], width=500)
-        st.write(f' :blue[**{df.columns[i]}**] \n :blue[Data distribution:]  {keys[result[i].item()]} \n :blue[95% Reference interval:]  [{lower[i]:.3f} - {upper[i]:.3f}] \n :blue[90% confidence interval:] [{lower90_low[i]:.3f}-{lower90_up[i]:.3f} ; {upper90_low[i]:.3f}-{upper90_up[i]:.3f}]')
+        st.write(f' :blue[**{df.columns[i]}**]  \n  :blue[Data distribution:]  {keys[result[i].item()]}  \n  :blue[95% Reference interval:]  [{lower[i]:.3f} - {upper[i]:.3f}]  \n  :blue[90% confidence interval:] [{lower90_low[i]:.3f}-{lower90_up[i]:.3f} ; {upper90_low[i]:.3f}-{upper90_up[i]:.3f}]')
