cc06cc19ab1c39f8a5624cb12fd4bfffbe3a9079
KevvLB <drleboedec@hotmail.fr>
2025-04-21 18:17:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -279,4 +279,4 @@ with tab1:
             st.image(hist[i], caption=df.columns[i], width=500)
             st.write(f' :blue[**{df.columns[i]}**]  \n  :blue[Data distribution:]  {keys[result[i].item()]}  \n  :blue[95% Reference interval:]  [{lower[i]:.3f} - {upper[i]:.3f}]  \n  :blue[90% Confidence intervals:] [{lower90_low[i]:.3f}-{lower90_up[i]:.3f} ; {upper90_low[i]:.3f}-{upper90_up[i]:.3f}]  \n  :blue[Statistical method for lower reference interval limit estimate:]  {method_lower}  \n  :blue[Statistical method for upper reference interval limit estimate:]  {method_upper}')
             if get_outlier(df.values[:,i])==True:
-                st.write(f' :red[Some values exceed Tukey's interquartile fences: doublecheck your data for potential outliers])
+                st.write(f' :red[Some values exceed Tukey's interquartile fences: doublecheck your data for potential outliers]')
