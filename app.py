86bf647a817621ecef67a0d2005f69a9ad6d8935
KevvLB <drleboedec@hotmail.fr>
2025-04-23 19:01:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,12 +25,6 @@ from streamlit_jupyter import StreamlitPatcher, tqdm
 import sys
 import streamlit.components.v1 as components
 
-# Insérer le tag Google Analytics
-with open("google-analytics.html", "r") as f:
-    html_code = f.read()
-components.html(html_code, height=0)
-
-
 # %% Projet 2_CNN model_HARISS_Shiny.ipynb 3
 tab1, tab2 = st.tabs(["App","Legal notice and disclosure"])
 
@@ -45,6 +39,11 @@ with tab2:
     st.markdown("The creator of HARISS is not to be held responsible for any clinical decisions that may come as a result of the model estimation. The reliability of a reference interval does not only depend on the statistical method used, but also on the quality of the data used to construct it.")
 
 with tab1:
+
+     with open("google-analytics.html", "r") as f:
+        html_code = f.read()
+    st.markdown(html_code, unsafe_allow_html=True)
+
     st.title(" :blue[HARISS] ")
     st.markdown(" :blue[Histogram Analyzer for Reference Intervals of Small Samples] ")
     
